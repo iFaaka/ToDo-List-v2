f41dd42febbb29763c3580b79b80fa89d71b184e
iFaaka <alvarezfacundo321@gmail.com>
2022-08-01 18:29:37 -0300

    CSS + Responsive

--- src/components/Tarea.jsx
+++ src/components/Tarea.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import './stylesheets/Tarea.css'
-import { RiDeleteBin7Line  } from "react-icons/ri";
+import React from "react";
+import "./stylesheets/Tarea.css";
+import { RiDeleteBin7Line } from "react-icons/ri";
 
-
-export const Tarea = ( { texto,id,handleDelete,completada,handleComplete } ) => {
-  
-
+export const Tarea = ({
+  texto,
+  id,
+  handleDelete,
+  completada,
+  handleComplete,
+}) => {
   return (
-    <div 
-    className='tarea-container'>
-        
-        <p 
-        className={`${completada == true ? 'tachado' : ''}`}
-        onClick={() => handleComplete(id)}>
-            {texto}
-        </p>
+    <div className="tarea-container">
+      <p
+        className={`${completada === true ? "tachado" : ""}`}
+        onClick={() => handleComplete(id)}
+      >
+        {texto}
+      </p>
 
-        <p
-            onClick={() => handleDelete(id)}>
-            <RiDeleteBin7Line  />
-        </p>
+      <p onClick={() => handleDelete(id)}>
+        <RiDeleteBin7Line />
+      </p>
     </div>
-  )
-}
+  );
+};
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,51 +1,49 @@
-import React from 'react'
-import { useState } from 'react'
-import { Formulario } from './Formulario'
-import { ListaDeTareas } from './ListaDeTareas'
-import './stylesheets/ToDoList.css'
+import React from "react";
+import { useState } from "react";
+import { Formulario } from "./Formulario";
+import { ListaDeTareas } from "./ListaDeTareas";
+import "./stylesheets/ToDoList.css";
 
 export const ToDoList = () => {
-    
-const [tareas, setTareas] = useState([])
- 
-const handleDelete = (id) => {
-    const newArr = tareas.filter(tarea => tarea.id !== id)
-    setTareas(newArr)
-  }
+  const [tareas, setTareas] = useState([]);
 
-const handleComplete = (id) => {
-    const newArr = tareas.map(tarea =>{
-        if(tarea.id == id && tarea.completada == false){
-           return {
-            texto: tarea.texto,
-            completada: true,
-            id:     tarea.id    }
-           
-        }else if(tarea.id == id && tarea.completada == true) {
-            return {
-                texto: tarea.texto,
-                completada: false,
-                id:     tarea.id    }
-        } else {
-            return tarea
-        }
-    })
+  const handleDelete = (id) => {
+    const newArr = tareas.filter((tarea) => tarea.id !== id);
+    setTareas(newArr);
+  };
 
-    setTareas(newArr)
-}
+  const handleComplete = (id) => {
+    const newArr = tareas.map((tarea) => {
+      if (tarea.id === id && tarea.completada === false) {
+        return {
+          texto: tarea.texto,
+          completada: true,
+          id: tarea.id,
+        };
+      } else if (tarea.id === id && tarea.completada === true) {
+        return {
+          texto: tarea.texto,
+          completada: false,
+          id: tarea.id,
+        };
+      } else {
+        return tarea;
+      }
+    });
 
+    setTareas(newArr);
+  };
 
+  return (
+    <div className="listadetareas-container">
+      <h1>Lista de tareas</h1>
 
-    return (
-    <div className='listadetareas-container'>
-        <h1>Lista de tareas</h1>
-
-        <Formulario onSubmit={setTareas}/>
-        <ListaDeTareas 
-        handleDelete={handleDelete} 
+      <Formulario onSubmit={setTareas} />
+      <ListaDeTareas
+        handleDelete={handleDelete}
         tareas={tareas}
-        handleComplete={handleComplete}/>
-
+        handleComplete={handleComplete}
+      />
     </div>
-  )
-}
+  );
+};
